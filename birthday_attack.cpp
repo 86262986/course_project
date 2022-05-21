d4f7b94a900b8b95a7f8da42cc752abdd0193fb5
Guoxia <105616449+YuxuanPeng@users.noreply.github.com>
2022-05-21 17:40:32 +0800

    birthday attack
    birthday attack to sm3

---
+++ birthday_attack.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include<windows.h> 
+#include"sm3.h"
+using namespace std;
+
+int main() {//主函数
+	int i = 100;
+	int j = 64;
+	map<string, int> hashMap;
+
+	double time = 0;
+	LARGE_INTEGER nFreq;
+	LARGE_INTEGER nBeginTime;
+	LARGE_INTEGER nEndTime;
+	QueryPerformanceFrequency(&nFreq);
+	QueryPerformanceCounter(&nBeginTime);//开始计时  
+
+	while (1) {
+		string n = DecToBin(i); //int i to binary string m
+		string m = BinToHex(n);
+		string paddingValue = padding(m);
+		string result = iteration(paddingValue); //SM3(m) value
+		string re = HexToBin(result);
+
+		//try to find collision
+		map<string, int>::iterator iter = hashMap.find(re.substr(0, j));
+		if (hashMap.end() == iter) {
+			hashMap[re.substr(0, j)] = i;
+		}
+		else {
+			QueryPerformanceCounter(&nEndTime);//停止计时  
+			cout << " Preimage of the first 64-bit collision: " << iter->second << " and " << i << endl;
+			break;
+		}
+		i++;
+	}
+	time = (double)(nEndTime.QuadPart - nBeginTime.QuadPart) / (double)nFreq.QuadPart;  
+	cout << "Time：" << time << "s" << endl;
+
+
+
+}
+
